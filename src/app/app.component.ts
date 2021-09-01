e4dcfd9f6e8bc0e4955e5d56fecb44dc7f7a4ae1
macbook <khaitruong1301@gmail.com>
2021-09-01 21:45:58 +0700

    buoi1_tao_component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'app-root', //tên thẻ
+  templateUrl: './app.component.html', //html của thẻ
+  styleUrls: ['./app.component.scss'] //css của thẻ
 })
 export class AppComponent {
   title = 'angularbc08e';
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DemoComponent } from './Components/Demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,DemoComponent //Nơi chứa component (Mỗi component sinh ra phải có module chứa nó)
   ],
   imports: [
-    BrowserModule
+    BrowserModule //Nơi import các module khác của ứng dụng
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], //Nơi khai báo các service 
+  bootstrap: [AppComponent] // Nơi khai báo các chạy tren index Component <app-root></app-root> được chạy trên index
 })
 export class AppModule { }
